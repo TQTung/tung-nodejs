56bd19ec5f8ac4ab7e4aeb2ae75c29e06254cfa6
TQTung <trinhquangtung99@gmail.com>
2022-11-12 13:43:39 +0700

    init express

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hihi");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World Im running Nodejs!");
+});
+app.get("/about", (req, res) => {
+  res.send("I'm running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port} ...`);
+});
